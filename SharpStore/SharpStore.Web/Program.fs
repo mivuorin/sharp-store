7aadcfbb54e2c31a676fef180df5d79b86af5fbb
mivuorin <mikko.h.vuorinen@gmail.com>
2024-07-05 10:45:39 +0300

    Hello Giraffe World!

---
+++ SharpStore/SharpStore.Web/Program.fs
@@ -0,0 +1,20 @@
+open System
+open Microsoft.AspNetCore.Builder
+open Microsoft.Extensions.Hosting
+open Giraffe
+
+let routes = choose [ route "/" >=> text "Hello Giraffe World!" ]
+
+[<EntryPoint>]
+let main args =
+
+    let builder = WebApplication.CreateBuilder(args)
+    
+    builder.Services.AddGiraffe() |> ignore
+    
+    let app = builder.Build()
+    
+    app.UseGiraffe(routes)      
+    app.Run()
+
+    0 // Exit code
