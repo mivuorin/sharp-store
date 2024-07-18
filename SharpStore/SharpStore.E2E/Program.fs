62baa92ca32b5895e8af32733507e31bda1a9874
mivuorin <mikko.h.vuorinen@gmail.com>
2024-07-18 12:42:08 +0300

    Canopy end 2 end test

---
+++ SharpStore/SharpStore.E2E/Program.fs
@@ -0,0 +1,32 @@
+﻿module SharpStore.E2E.Program
+
+open System
+open canopy
+open canopy.classic
+open canopy.runner.classic
+
+let index_test () =
+
+    context "Index"
+
+    "hello canopy test"
+    &&& fun _ ->
+
+        url "https://localhost:7285"
+
+        "h1" == "Welcome to Giraffe"
+
+        waitFor (fun () -> title () = "SharpStore")
+
+
+[<EntryPoint>]
+let main _ =
+    start chrome
+
+    index_test ()
+
+    run ()
+
+    quit ()
+
+    0
